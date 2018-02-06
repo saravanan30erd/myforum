10309a2317bb4ba18ac69179c52f8f465e004edb
saravanan30erd <saravanan30erd@gmail.com>
2018-02-06 16:54:01 +0400

    added user authentication, password reset & change

--- boards/views.py
+++ boards/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import Http404
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 from .models import Board, Topic, Post
 from .forms import NewTopicForm
 
@@ -15,22 +16,26 @@ def board_topics(request, pk):
         raise Http404
     return render(request, 'topics.html', {'board' : board})
 
+@login_required
 def new_topic(request, pk):
     board = get_object_or_404(Board, pk=pk)
-    user = User.objects.first() #Get the currently logged in user
     if request.method == 'POST':
         form = NewTopicForm(request.POST)
         if form.is_valid():
             topic = form.save(commit=False)
             topic.board = board
-            topic.starter = user
+            topic.starter = request.user
             topic.save()
             post = Post.objects.create(
                 message=form.cleaned_data.get('message'),
                 topic=topic,
-                created_by=user
+                created_by=request.user
             )
             return redirect('board_topics', pk=board.pk) #Redirect to the created topic page
     else:
         form = NewTopicForm()
     return render(request, 'new_topic.html', {'board' : board, 'form': form})
+
+def topic_posts(request, pk, topic_pk):
+    topic = get_object_or_404(Topic, board__pk=pk, pk=topic_pk)
+    return render(request, 'topic_posts.html', {'topic': topic})
